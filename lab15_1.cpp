93cb35808fadfb21feea74f362c73ab0e66e93a2
Phummyphum <110968491+Phummyphum@users.noreply.github.com>
2023-01-17 16:54:04 +0700

    Update lab15_1.cpp

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
 using namespace std;
+template <typename T>
+void show(T d[],int i){
+  cout << "Pass " << i << ":";
+  for(int x=0;x<10;x++){
+    cout << d[x] << " ";
+  }
+  cout << "\n";
+}
 
 template <typename T>
+void swap(T d[],int i){
+  for(int x=i;x>0;x--){
+    T temp =d[x];
+    if(d[x]>d[x-1]){
+      d[x] = d[x-1];
+      d[x-1]=temp;
+    }
+  }
+}
+template <typename T>
 void insertionSort(T d[],int N){
-
+  for(int i=1; i<N; i++){
+    swap(d,i);
+    show(d,i);
+  }
 }
 
 int main(){
